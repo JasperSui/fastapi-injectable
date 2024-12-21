5ac77beb945ad9206544957c9bffb6f18abf264b
Jasper Sui <suiyang03@gmail.com>
2024-12-21 13:42:58 +0800

    fix(coverage): use --parellel-mode instead of --parallel for coverage run

--- noxfile.py
+++ noxfile.py
@@ -136,7 +136,7 @@ def tests(session: Session) -> None:
     session.install(".")
     session.install("coverage[toml]", "pytest", "pytest-asyncio")
     try:
-        session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
+        session.run("coverage", "run", "--parallel-mode", "-m", "pytest", *session.posargs)
     finally:
         if session.interactive:
             session.notify("coverage", posargs=[])
